020f498a927f3c9b3c8e0af7c6840f5c80818ec7
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-16 11:37:39 +0330

    feat: add Icons to Sidebar template

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,31 +1,56 @@
-import "./Sidebar.css"
+import { AiOutlineHome } from "react-icons/ai";
+import { MdOutlineProductionQuantityLimits } from "react-icons/md";
+import { MdOutlineInsertComment } from "react-icons/md";
+import { FiUsers } from "react-icons/fi";
+import { MdOutlineShoppingBag } from "react-icons/md";
+import { BsCurrencyDollar } from "react-icons/bs";
+
+import "./Sidebar.css";
 
 function Sidebar() {
   return (
     <div className="sidebar">
-        <h1 className="sidebar-title">به داشبورد خود خوش آمدید</h1>
-        <ul className="sidebar-links">
-            <li className="active">
-                <a href="#">صفحه اصلی</a>
-            </li>
-            <li>
-                <a href="#">محصولات</a>
-            </li>
-            <li>
-                <a href="#">کامنت ها</a>
-            </li>
-            <li>
-                <a href="#">کاربران</a>
-            </li>
-            <li>
-                <a href="#">سفارشات</a>
-            </li>
-            <li>
-                <a href="#">تخفیف ها</a>
-            </li>
-        </ul>
+      <h1 className="sidebar-title">به داشبورد خود خوش آمدید</h1>
+      <ul className="sidebar-links">
+        <li className="active">
+          <a href="#">
+            <AiOutlineHome />
+            صفحه اصلی
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <MdOutlineProductionQuantityLimits />
+            محصولات
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <MdOutlineInsertComment />
+            کامنت ها
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <FiUsers />
+            کاربران
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <MdOutlineShoppingBag />
+            سفارشات
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <BsCurrencyDollar />
+            تخفیف ها
+          </a>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Sidebar
+export default Sidebar;
