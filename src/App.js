8adb1dc4b17b5e4e5cbaf945cd627d9e57390fb9
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-17 11:17:06 +0330

    refactor: routing structure

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useRoutes } from "react-router-dom";
 
 import Sidebar from "./components/Sidebar/Sidebar";
 import Header from "./components/Header/Header";
-import Products from "./components/Products/Products";
-import Comments from "./components/Comments/Comments";
-import Users from "./components/Users/Users";
-import Orders from "./components/Orders/Orders";
-import Offs from "./components/Offs/Offs";
+import routes from "./routes";
 
 import "./App.css";
 
 export default function App() {
+  const router = useRoutes(routes);
+
   return (
     <>
-      <Routes>
-        <Route path="/products" element={<Products />} />
-        <Route path="/comments" element={<Comments />} />
-        <Route path="/users" element={<Users />} />
-        <Route path="/orders" element={<Orders />} />
-        <Route path="/offs" element={<Offs />} />
-      </Routes>
       <Sidebar />
       <div className="main">
         <Header />
+        {router}
       </div>
     </>
   );
---
+++ src/routes.js
@@ -0,0 +1,30 @@
+import Products from "./components/Products/Products";
+import Comments from "./components/Comments/Comments";
+import Users from "./components/Users/Users";
+import Orders from "./components/Orders/Orders";
+import Offs from "./components/Offs/Offs";
+
+const routes = [
+  {
+    path: "/products",
+    element: <Products />,
+  },
+  {
+    path: "/comments",
+    element: <Comments />,
+  },
+  {
+    path: "/orders",
+    element: <Orders />,
+  },
+  {
+    path: "/users",
+    element: <Users />,
+  },
+  {
+    path: "/offs",
+    element: <Offs />,
+  },
+];
+
+export default routes;
