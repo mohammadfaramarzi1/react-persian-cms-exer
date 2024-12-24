b874349edd396b9a108bca85edec2726069b5f50
mohammadfaramarzi1 <faramarzi.dev@gamil.com>
2024-12-24 11:30:46 +0330

    feat: Commnets table template

--- frontend/src/components/Comments/Comments.js
+++ frontend/src/components/Comments/Comments.js
@@ -1,12 +1,56 @@
-import React from 'react'
-import Errorbox from '../Errorbox/Errorbox'
+import { useState, useEffect } from "react";
+
+import Errorbox from "../Errorbox/Errorbox";
 
 function Comments() {
+  const [allComments, setAllComments] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:8000/api/comments/")
+      .then((res) => res.json())
+      .then((comments) => {
+        setAllComments(comments);
+      });
+  }, []);
+
   return (
-    <div>
-      <Errorbox msg="هیچ کامنتی یافت نشد..." />
+    <div className="cms-main">
+      {allComments.length ? (
+        <table className="cms-table">
+          <thead>
+            <tr>
+              <th>نام کاربر</th>
+              <th>نام محصول</th>
+              <th>متن کامنت</th>
+              <th>تاریخ ثبت</th>
+              <th>ساعت ثبت</th>
+            </tr>
+          </thead>
+          <tbody>
+            {allComments.map((comment) => (
+              <tr key={comment.id}>
+                <td>{comment.userID}</td>
+                <td>{comment.productID}</td>
+                <td>
+                  <button>دیدن محتوا</button>
+                </td>
+                <td>{comment.date}</td>
+                <td>{comment.hour}</td>
+                <td>
+                  <button>حذف</button>
+                  <button>ویرایش</button>
+                  <button>پاسخ</button>
+                  <button>تایید</button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      ) : (
+        <Errorbox msg="هیچ کامنتی یافت نشد..." />
+      )}
     </div>
-  )
+  );
 }
 
-export default Comments
+export default Comments;
